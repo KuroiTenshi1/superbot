438227ca19d2bcef1506a935e9e5d4a0e4b13e52
dchiquet <danny77c@gmail.com>
2017-07-20 17:32:48 +0200

    index

---
+++ index.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('message', function (message){
+    if (message.content === '%ping'){
+        message.channel.send('pong')
+    }
+})
+/*
+bot.on('message', function (message){
+    if (message.content === '%membre'){
+        var number = guild.memberCount
+        message.channel.send('nombre de membre '+Discord.guild)
+    }
+})
+*/
+
+bot.login('token')
+
+

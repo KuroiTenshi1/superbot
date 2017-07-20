b1e5cf63add9ae102a394f3cebd7c461e6fa5c22
dchiquet <danny77c@gmail.com>
2017-07-20 21:45:23 +0200

    vote jour guild

--- index.js
+++ index.js
@@ -1,23 +1,37 @@
 const Discord = require('discord.js')
+const guild = new  Discord.Guild()
 const bot = new Discord.Client()
-const guild =
+
+
+bot.on('ready', function(){
+    bot.user.setAvatar('./img/partial-solar-eclipse-clouds.jpg').catch(console.error)
+    bot.user.setGame('niquer des mères').catch(console.error)
+    bot.user.setUsername('Eclipse').catch(console.error)
+})
+
+
 bot.on('message', function (message){
     if (message.content === 'lg!ping'){
         message.channel.send('pong')
     }
-    /*
-    if (message.content === '%membre'){
+    // ne marche pas
+    if (message.content === 'lg!membre'){
         var number = guild.memberCount
-        message.channel.send('nombre de membre '+Discord.guild)
+        message.channel.send('nombre de membre '+number)
     }
-    */
     if (message.content === 'lg!nuit'){
-        message.channel.send('La nuit tombe les @everyone s\'endorment')
+        message.channel.send('La nuit tombe les \@Villageois s\'endorment')
+    }
+    if (message.content === 'lg!jour'){
+        message.channel.send('La jour ce l\ève les \@Villageois se r\éveille')
+    }
+    if (message.content === 'lg!timer'){
+        message.channel.send('\@Villageois \r Vous avez \"temps\" minutes pour d\ébattre #debat')
+    }
+    if (message.content === 'lg!vote'){
+        message.channel.send('\@Villageois \r C\'est l\'heure des votes \#vote')
     }
 
-    if (message.content === 'lg!timer'){
-        message.channel.send('Vous avec \"temps\" minutes pour d\êbatre')
-    }
 
 
 })

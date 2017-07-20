f0c160f7c818df75942f405a0b67f566b44216b1
dchiquet <danny77c@gmail.com>
2017-07-20 18:11:50 +0200

    timer et nuit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
-
+const guild =
 bot.on('message', function (message){
-    if (message.content === '%ping'){
+    if (message.content === 'lg!ping'){
         message.channel.send('pong')
     }
     /*
@@ -11,6 +11,15 @@ bot.on('message', function (message){
         message.channel.send('nombre de membre '+Discord.guild)
     }
     */
+    if (message.content === 'lg!nuit'){
+        message.channel.send('La nuit tombe les @everyone s\'endorment')
+    }
+
+    if (message.content === 'lg!timer'){
+        message.channel.send('Vous avec \"temps\" minutes pour d\êbatre')
+    }
+
+
 })
 
 

7d61f19cf4b85e7c960ccfe13d2530471440b2ab
dchiquet <danny77c@gmail.com>
2017-07-21 17:27:13 +0200

    bienvenue; commands

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const Discord = require('discord.js')
-const guild = new  Discord.Guild()
 const bot = new Discord.Client()
 var jour = 1
 
 bot.on('ready', function(){
-    bot.user.setAvatar('./img/partial-solar-eclipse-clouds.jpg').catch(console.error)
-    bot.user.setGame('niquer des mères').catch(console.error)
-    bot.user.setUsername('Eclipse').catch(console.error)
+    //bot.user.setAvatar('./img/partial-solar-eclipse-clouds.jpg').catch(console.error)
+    //bot.user.setUsername('Eclipse').catch(console.error)
+    bot.user.setGame('En pr\éparation').catch(console.error)
+})
+
+
+bot.on('guildMemberAdd', function(member){
+    member.createDM().then(function(channel){
+        return channel.send('Bienvenue sur le serveur '+member.displayName)
+    }).catch(console.error)
 })
 
 
 bot.on('message', function (message){
 
+    //Message écrit
     if (message.content === 'lg!ping'){
         message.channel.send('pong')
-    }
-    // ne marche pas
-    if (message.content === 'lg!membre'){
-        var number = guild.memberCount
-        message.channel.send('nombre de membre '+number)
-    }
-    if (message.content === 'lg!dispoguild'){
-        var number = guild.available
-        message.channel.send('disp '+number)
     }
     if (message.content === 'lg!nuit'){
         message.channel.send('La nuit tombe les \@Villageois s\'endorment')
@@ -43,6 +41,16 @@ bot.on('message', function (message){
     if(message.content === 'lg!fin'){
         message.channel.send('\@Villageois \r C\'est la fin')
         jour=1
+    }
+    //Gestion du serveur
+    // ne marche pas
+    if (message.content === 'lg!membre'){
+        var number = guild.memberCount
+        message.channel.send('nombre de membre '+number)
+    }
+    if (message.content === 'lg!dispoguild'){
+        var number = guild.available
+        message.channel.send('disp '+number)
     }
 
 

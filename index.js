488e00fbacd0f792d853d1f86b58da7a337509cf
KuroiTenshi1 <chiquet.daniel@gmail.com>
2017-08-09 21:03:26 +0200

    tableau roles

--- index.js
+++ index.js
@@ -7,7 +7,35 @@ const ft = require('./functions/ft')
 var jour = 1
 var players = [];
 var names=[];
-var roles=[];
+var roles={
+  "loup" : "336910690361016330",
+  "voyante" : "336910820539760650",
+  "sorciere" :"336910769742544899",
+  "cupidon" :"336965266342805504",
+  "Petite fille" :"336910803745636353",
+  "chasseur" :"336910847005818881",
+  "voleur" :"336910861123977218",
+  "salvateur" :"336910791167180801",
+  "idiot du village" :"336910875824750593",
+  "bouc emissaire" :"336910894426750978",
+  "ancien" :"336910912479035393",
+  "joueur de flute" :"336953910482829312",
+  "corbeau" :"336953930900832256",
+  "loup blanc" :"336954044470001681",
+  "ange" :"336954149595774978",
+  "alchimiste" :"336954211113893888",
+  "moine" :"336954260430389248",
+  "arnacoeur" :"336954305519288333",
+  "avocat" :"336954350004076545",
+  "berger" :"336954393482231810",
+  "chaman" :"336954428294955011",
+  "chien" :"336960830174265357",
+  "enfant sauvage" :"336954548822474753",
+  "renard" :"336954670348238848",
+  "servante dévouée" :"336954697166356490",
+  "frere" :"336954769077829632",
+  "soeur" :"336954804003667969"
+};
 
 //OBJECT CONSTRUCTOR - Player
 function player(pseudo, role, alive) {
@@ -82,7 +110,11 @@ function salon(room){
         salon('Village').send('<@&336273519749103617> \r C\'est la fin de la partie')
         return jour=1
     }
-    
+    if(message.content === 'lg!role'){
+      var nom_du_role = "loup"
+      var msg = "<@&"+ roles[nom_du_role]+">"
+     	message.channel.send(msg)
+    }
     
     //Pour sinscrire dans le loup-garou et creer un objet Joueur
     if(message.content === 'lg!register'){
